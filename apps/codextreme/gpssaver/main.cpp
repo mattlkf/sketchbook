2721d127a000d6adc9764b42d9155ef36f2b94c4
Matthew Lee <mattlkf@outlook.com>
2014-07-11 16:01:41 +0800

    make gpssaver echo GPS input (copied from ex.)

--- apps/codextreme/gpssaver/main.cpp
+++ apps/codextreme/gpssaver/main.cpp
@@ -4,8 +4,80 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_GPS.h>
 
+
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  true
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
 void setup(){
-	Serial.begin(9600);
+	// connect at 115200 so we can read the GPS fast enuf and
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic test!");
+
+  // 9600 NMEA is the default baud rate for MTK - some use 4800
+  GPS.begin(9600);
+  
+  // You can adjust which sentences to have the module emit, below
+  
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data for high update rates!
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // uncomment this line to turn on all the available data - for 9600 baud you'll want 1 Hz rate
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
+  
+  // Set the update rate
+  // 1 Hz update rate
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  // 5 Hz update rate- for 9600 baud you'll have to set the output to RMC or RMCGGA only (see above)
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  // 10 Hz update rate - for 9600 baud you'll have to set the output to RMC only (see above)
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+  
+  delay(1000);
+
+
 }
 
 int main(){
