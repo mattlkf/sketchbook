8e96a4d14fa338e77f13ad06360093182c7eb3f0
Matthew Lee <mattlkf@outlook.com>
2014-07-15 16:37:44 +0800

    [cheesesock, csockr] Trim down CheeseSock, write example
    I removed a lot of the cruft from CheeseSock that was
    SnS-related. I modified it to use only SoftwareSerial.
    I removed the singleton pattern and used the style of
    the Adafruit GPS code - no more caling getInstance().
    I removed the registerAppFn and we can now only register
    non-member functions. Lots of changes but the basic
    functionality remains.
    
    csockr is example code. Demonstrates escaping of slash.

--- libraries/CheeseSock/CheeseSock.cpp
+++ libraries/CheeseSock/CheeseSock.cpp
@@ -1,65 +1,32 @@
 /* 
- * \file    CheeseSock.cpp
+ * \file    CheeseSock.h
  * \ingroup 
  * \author  mattlkf
- * \date    2014-03-31
+ * \date    2014-03-29, 2014-07-15
  *
- * A little thing to separate SenseActuateModule from the UniNet stack
- * Frames serial communications using SLIP-like escapes
- * Allows apps to register different member recv fns to different ports
- * Based on Alvin's LightSock
+ * *Much* littler than the CheeseSock I wrote for SnS
  */
 
 #include <CheeseSock.h>
 
-CheeseSock* CheeseSock::instance_ = NULL;
-
 const char CheeseSock::preamble[] = "/<" , CheeseSock::postamble[] = "/>";
 
-CheeseSock::CheeseSock(){
+CheeseSock::CheeseSock(SoftwareSerial * ser){
 	read_state = discard;
 	esc_state = normal;
 	debug_state = none;
 
 	for(uint8_t i = 0; i < CSOCK_MAX_PORTS; i++){
-		csapp[i] = NULL;
 		fp[i] = NULL;
 	}
+
+	sp = ser;
 }
 
-#ifdef __linux__
-void CheeseSock::init(const char * tty, uint32_t baudrate){
-	sp = new SerialPort_LibSerial(tty);
-	
-    if(!(baudrate == 115200 || baudrate == 57600)){
-		printf("Please choose baudrate 57600 or 115200\n");
-		baudrate = 57600;
-	}
-
-	sp->begin(baudrate);
-
-	printf("Listening on serial %s at %d\n", tty, baudrate);
-	return;
-}
-#else
-void CheeseSock::init(){
-	sp = &gSerial0;
-
-	return;
-}
-#endif
-
-void CheeseSock::registerApp(CheeseSockApp * app){
-	// default: register the recv() function of the app to the port.
-	registerAppFn(app->getPort(), app, &CheeseSockApp::recv);
-	return;
-}
-
-void CheeseSock::registerAppFn(uint8_t port, CheeseSockApp * app, CheeseSockApp::RecvFn fn){
+void CheeseSock::registerFn(uint8_t port, void (*fn)(const uint8_t *, uint8_t)){
 	if(port < 0 || port >= CSOCK_MAX_PORTS) return; //port does not exist
-	if(csapp[port] != NULL || fp[port] != NULL) return; //port is occupied
+	if(fp[port] != NULL) return; //port is occupied
 
-	csapp[port] = app;
 	fp[port] = fn;
 	return;
 }
@@ -69,41 +36,28 @@ void CheeseSock::handleFrame(const uint8_t * str, const uint8_t len){
 
 	uint8_t port = str[0];
 
-	uint16_t src = 0; //Matt: temporary. Do CheeseSockApps have a notion of source?
-	(csapp[port]->*fp[port])(src, (const char*) str+1, len-1);
+	fp[port](str + 1, len-1);
 	return;
 }
 
 void CheeseSock::run(){
-//	gSerial0.write("hi\n");
 	readloop();
 	return;
 }
 
-// type are for compatibility with NetworkApp
-int8_t CheeseSock::send(CheeseSockApp * app, const char * msg, int len){
-	if(len > 255) return -1; // we don't send such long messages!
-
-	send(app->getPort(), (const uint8_t *) msg, (uint8_t) len);
-	return 0;
+void CheeseSock::send(uint8_t port, const char * str){
+	send(port, (const uint8_t *) str, strlen(str));
 }
 
 void CheeseSock::send(uint8_t port, const uint8_t * msg, uint8_t len){
-	uint16_t csum = checksum(msg, len);
-
 	sp->write(postamble);
 	sp->write(preamble);
-	escapify((const uint8_t *) &csum, 2, '/'); 
 	escapify((const uint8_t *) &port, 1, '/');
 	escapify(msg, len, '/');
 	sp->write(postamble);
 
 	sp->write('\n');
 	return;
-}
-
-uint16_t CheeseSock::checksum(const uint8_t * str, const uint8_t len){
-	return (uint16_t) 90*256+90;
 }
 
 void CheeseSock::escapify(const uint8_t * str, const uint8_t len, const uint8_t esc){
@@ -125,17 +79,16 @@ void CheeseSock::begin_slip(){
 
 void CheeseSock::end_slip(){
 	read_state = discard;
-	if(len < 2) return;
-	//Todo: only pass the frame to handleFrame if it passes the checksum
-	handleFrame(buf + 2, len - 2);
+	handleFrame(buf, len);
 	len = 0;
 }
 
 /********** buffering *********************/
 void CheeseSock::addtobuf(uint8_t ch){
 	buf[len++] = ch;
-	if(len == MAX_BUF){
+	if(len == CSOCK_MAX_BUF){
 		len = 0; //empty the full buffer
+		Serial.println("Overflew");
 		//Todo: raise an error on overflow
 	}
 }
@@ -147,7 +100,7 @@ void CheeseSock::readloop(){
 
 	uint8_t ch = sp->read();
 
-	if(debug_state == all) printf("%c", ch);
+	if(debug_state == all) Serial.write(ch);
 
 	if(esc_state == normal){
 		if(ch == '/') esc_state = escape;
--- libraries/CheeseSock/CheeseSock.h
+++ libraries/CheeseSock/CheeseSock.h
@@ -2,73 +2,68 @@
  * \file    CheeseSock.h
  * \ingroup 
  * \author  mattlkf
- * \date    2014-03-29
+ * \date    2014-03-29, 2014-07-15
  *
- * A little thing to separate SenseActuateModule from the UniNet stack
- * Frames serial communications using SLIP-like escapes
- * Allows apps to register different member recv fns to different ports
- * Based on Alvin's LightSock
+ * *Much* littler than the CheeseSock I wrote for SnS
  */
 
 #ifndef _CHEESESOCK_H
 #define	_CHEESESOCK_H
 
-#define CSOCK_MAX_PORTS 16
-#define MAX_BUF 256
-
-#include "CheeseSockApp.h"
-
-#ifdef __linux__
- 	#include <serialport_libserial.h>
-#else
-	#include <serialport.h>
+#ifndef CSOCK_MAX_PORTS
+	#define CSOCK_MAX_PORTS 16
 #endif
 
-#include <node.h>
+#ifndef CSOCK_MAX_BUF
+	#define CSOCK_MAX_BUF 128
+#endif
+
+#include <Arduino.h>
+#include "SoftwareSerial.h"
 
 class CheeseSock
 {
 
 public:
-	// Serial stuff: on Linux, you can choose the tty_dev to use.
-	#ifdef __linux__
-		void init(const char * tty, uint32_t baudrate);
-		void printAll(){ debug_state = all; }
-	#else
-		void init();
-	#endif
-	
-	static CheeseSock* getInstance() { return instance_ ? instance_ : ( instance_ = new CheeseSock()); }
+	CheeseSock(SoftwareSerial *);
+
+	void begin(uint16_t baud){
+		if(!(baud == 9600 || baud == 57600)){
+			return;
+		}
+
+		sp->begin(baud);
+	}
+
+	void begin(uint16_t baud, uint8_t debug){
+		if(debug == 0)
+			debug_state = none;
+		else 
+			debug_state = all;
+
+		begin(baud);
+	}
 
 	void run();
-	
-	void registerApp(CheeseSockApp * app);
-	void registerAppFn(uint8_t port, CheeseSockApp * app, CheeseSockApp::RecvFn);
 
-	int8_t send(CheeseSockApp * app, const char * msg, int len);
+	void registerFn(uint8_t port, void(*)(const uint8_t *, uint8_t));
+
 	void send(uint8_t port, const uint8_t * msg, uint8_t len);
+	void send(uint8_t port, const char * msg);
 
 private:
-	// Serial stuff: on Linux, we use the LibSerial-based version.
-	#ifdef __linux__
-		SerialPort_LibSerial * sp;
-	#else
-		SerialPort * sp;
-	#endif
-
-	static CheeseSock* instance_;
+	SoftwareSerial * sp;
 
 	// App and recv function registry
-	CheeseSockApp * csapp[CSOCK_MAX_PORTS];
-	CheeseSockApp::RecvFn fp[CSOCK_MAX_PORTS];
-
-	CheeseSock();
+	void (*fp[CSOCK_MAX_PORTS])(const uint8_t *, uint8_t);
 
 	enum { none, all } debug_state;
 	enum { discard, save } read_state;
 	enum { escape, normal } esc_state;
+
 	static const char preamble[], postamble[];
-	uint8_t buf[MAX_BUF];
+
+	uint8_t buf[CSOCK_MAX_BUF];
 	uint8_t len;
 
 	void escapify(const uint8_t * str, const uint8_t len, const uint8_t esc);
